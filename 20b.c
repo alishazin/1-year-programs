8b191ee0e14cb2c2304657dedcf50bea96e00af6
alishazin <78612608+alishazin@users.noreply.github.com>
2023-01-02 11:09:36 +0530

    made changes

--- 20b.c
+++ 20b.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 float findSum(float num1, float num2) {
-    float sum = num1 + num2;
-    return sum;
-}
-
-int main() {
-
     float num1, num2, sum;
 
     printf("\nEnter two numbers to find sum: ");
     scanf("%f %f", &num1, &num2);
+    sum = num1 + num2;
+    return sum;
+}
+
+int main() {
 
     sum = findSum(num1, num2);
 
