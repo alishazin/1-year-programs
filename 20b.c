212e07d3b97da93197b0089fc3ccccf7aab734bc
alishazin <78612608+alishazin@users.noreply.github.com>
2023-01-02 11:22:32 +0530

    some shit

--- 20b.c
+++ 20b.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float findSum(float num1, float num2) {
+float findSum() {
     float num1, num2, sum;
 
     printf("\nEnter two numbers to find sum: ");
@@ -11,7 +11,7 @@ float findSum(float num1, float num2) {
 
 int main() {
 
-    sum = findSum(num1, num2);
+    float sum = findSum(num1, num2);
 
     printf("\nSum: %0.2f", sum);
 
